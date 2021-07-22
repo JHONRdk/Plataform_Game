de13400652d6daaf216a6264de03522a7852282a
JHONRdk <87137935+JHONRdk@users.noreply.github.com>
2021-07-21 23:19:14 -0500

    Create trackKeys.js

---
+++ src/trackKeys.js
@@ -0,0 +1,19 @@
+export function trackKeys(keys) {
+  let down = Object.create(null);
+  function track(event) {
+    console.log(keys);
+    if (keys.includes(event.key)) {
+      down[event.key] = event.type === "keydown";
+      event.preventDefault();
+    }
+  }
+  window.addEventListener("keydown", track);
+  window.addEventListener("keyup", track);
+
+  down.unregister = () => {
+    window.removeEventListener("keydown", track);
+    window.removeEventListener("keyup", track);
+  };
+
+  return down;
+}
