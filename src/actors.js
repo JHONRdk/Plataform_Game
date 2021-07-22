04504de68617ff9077666066dad963dbc671ed38
JHONRdk <87137935+JHONRdk@users.noreply.github.com>
2021-07-21 23:35:09 -0500

    Update actors.js

--- src/actors.js
+++ src/actors.js
@@ -15,3 +15,53 @@ export class Player {
     return new Player(pos.plus(new Vec(0, -0.5)), new Vec(0, 0));
   }
 }
+
+const playerXSpeed = 7;
+const gravity = 30;
+const jumpSpeed = 17;
+
+Player.prototype.update = function(time, state, keys) {
+  let xSpeed = 0;
+  if (keys.ArrowLeft) xSpeed -= playerXSpeed;
+  if (keys.ArrowRight) xSpeed += playerXSpeed;
+  let pos = this.pos;
+  let movedX = pos.plus(new Vec(xSpeed * time, 0));
+  if (!state.level.touches(movedX, this.size, "wall")) {
+    pos = movedX;
+  }
+
+  let ySpeed = this.speed.y + time * gravity;
+  let movedY = pos.plus(new Vec(0, ySpeed * time));
+  if (!state.level.touches(movedY, this.size, "wall")) {
+    pos = movedY;
+  } else if (keys.ArrowUp && ySpeed > 0) {
+    ySpeed = -jumpSpeed;
+  } else {
+    ySpeed = 0;
+  }
+  return new Player(pos, new Vec(xSpeed, ySpeed));
+};
+
+Player.prototype.size = new Vec(0.8, 1.5);
+
+export class Lava {
+  constructor(pos, speed, reset) {
+    this.pos = pos;
+    this.speed = speed;
+    this.reset = reset;
+  }
+
+  get type() {
+    return "lava";
+  }
+
+  static create(pos, ch) {
+    if (ch === "=") {
+      return new Lava(pos, new Vec(2, 0));
+    } else if (ch === "|") {
+      return new Lava(pos, new Vec(0, 2));
+    } else if (ch === "v") {
+      return new Lava(pos, new Vec(0, 3), pos);
+    }
+  }
+}
