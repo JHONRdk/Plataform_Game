61547e3efefa63930e8b1c30a725dd5d33e72ef8
JHONRdk <87137935+JHONRdk@users.noreply.github.com>
2021-07-21 23:29:57 -0500

    Create actors.js

---
+++ src/actors.js
@@ -0,0 +1,17 @@
+import { Vec } from "./vector";
+import { State } from "./state";
+
+export class Player {
+  constructor(pos, speed) {
+    this.pos = pos;
+    this.speed = speed;
+  }
+
+  get type() {
+    return "player";
+  }
+
+  static create(pos) {
+    return new Player(pos.plus(new Vec(0, -0.5)), new Vec(0, 0));
+  }
+}
