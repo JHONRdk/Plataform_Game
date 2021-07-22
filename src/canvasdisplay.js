7903aabd3be0277597c2d20da61664d70d451bce
JHONRdk <87137935+JHONRdk@users.noreply.github.com>
2021-07-22 13:33:04 -0500

    Update canvasdisplay.js

--- src/canvasdisplay.js
+++ src/canvasdisplay.js
@@ -53,3 +53,64 @@ CanvasDisplay.prototype.updateViewport = function(state) {
     );
   }
 };
+
+CanvasDisplay.prototype.clearDisplay = function(status) {
+  if (status === "won") {
+    this.cx.fillStyle = "rgb(68, 191, 255)";
+  } else if (status === "lost") {
+    this.cx.fillStyle = "rgb(44, 136, 214)";
+  } else {
+    this.cx.fillStyle = "rgb(52, 166, 251)";
+  }
+  this.cx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+};
+
+let otherSprites = document.createElement("img");
+otherSprites.src = "img/sprites.png";
+
+CanvasDisplay.prototype.drawBackground = function(level) {
+  let { left, top, width, height } = this.viewport;
+  let xStart = Math.floor(left);
+  let xEnd = Math.ceil(left + width);
+  let yStart = Math.floor(top);
+  let yEnd = Math.ceil(top + height);
+
+  for (let y = yStart; y < yEnd; y++) {
+    for (let x = xStart; x < xEnd; x++) {
+      let tile = level.rows[y][x];
+      if (tile === "empty") continue;
+      let screenX = (x - left) * scale;
+      let screenY = (y - top) * scale;
+      let tileX = tile === "lava" ? scale : 0;
+      this.cx.drawImage(
+        otherSprites,
+        tileX,
+        0,
+        scale,
+        scale,
+        screenX,
+        screenY,
+        scale,
+        scale
+      );
+    }
+  }
+};
+
+let playerSprites = document.createElement("img");
+playerSprites.src = "img/player.png";
+const playerXOverlap = 4;
+
+CanvasDisplay.prototype.drawPlayer = function(player, x, y, width, height) {
+  width += playerXOverlap * 2;
+  x -= playerXOverlap;
+  if (player.speed.x !== 0) {
+    this.flipPlayer = player.speed.x < 0;
+  }
+
+  let tile = 8;
+  if (player.speed.y !== 0) {
+    tile = 9;
+  } else if (player.speed.x !== 0) {
+    tile = Math.floor(Date.now() / 60) % 8;
+  }
